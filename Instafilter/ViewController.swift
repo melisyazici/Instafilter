9f4cfa88b1ff854ccde1eb2dce244780e098828f
Melis Yazıcı <melis.yazici95@icloud.com>
2022-11-18 15:13:01 +0100

    Design Interface

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var filterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        
+    }
+    
 
 }
 
